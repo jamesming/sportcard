25267b59785c2138a5fddbe483bca5432d857d72
James Ming <jamesming@gmail.com>
2011-12-31 12:58:01 -0800

    added

--- application/views/home/index_view.php
+++ application/views/home/index_view.php
@@ -34,18 +34,7 @@
 	div#main-box{
 	min-height:600px;
 	}
-	#head-line-box{
-	width: 500px; 
-	height: 200px;	
-	display:none;	
-	}
-	
-	#head-line-box #full_name_readonly{
-	font-size:15px;
-	font-weight:bold;
-	color:gray;	
-	padding-left:10px;
-	}
+
 	.draggable{ 
 	float: left;
 	background:transparent;	
@@ -66,17 +55,50 @@
 	.draggable div.close-window {
 	background: url("http://localhost/sportcard/images/icons.png") no-repeat scroll -14px -172px yellow;
 	cursor: pointer; 
-	}	
-	#control-panel-box{
-	position:relative;
-	top:100px;
-	left:100px;
-	background:red;
-	width:300px;
-	height: 300px;			
 	}
-	#control-panel-box table{
-	padding:20px;	
+	#head-line-box{
+	width: 500px; 
+	height: 200px;	
+	position:absolute;
+	}
+	
+	#head-line-box #full_name_readonly{
+	font-size:15px;
+	font-weight:bold;
+	color:gray;	
+	padding-left:10px;
+	}
+	.window-controls-container{
+	height:21px;
+	background:gray;
+	}
+	#control-panel-box{
+	position:absolute;
+	top:100px;
+	left:320px;
+	background:red;
+	width:406px;
+	height: 300px;			
+	}		
+		#control-panel-box #panel-tabs_container{
+		height:19px;
+		background:lightblue;	
+		}
+		#control-panel-box #panel-tabs_container div{
+			float:left;
+			border-right:1px solid gray;
+			width:100px;
+			border-bottom:1px solid gray;
+		}		
+
+		#control-panel-box #control-panel-box table{
+		padding:20px;	
+		}
+	#control-panel-box .panel{
+	display:none;	
+	}
+	#control-panel-box #panel-1{
+	display:block;	
 	}
 </style>
 </head>
@@ -86,9 +108,16 @@
 <body>
 <div  id='header' class='container ' >
 	<div class='float_left coordinates '>
+		<style>
+		#form0 input{
+		width:30px;	
+		}
+		</style>
 		<form id='form0'>
 			<input name="x" id="x" type="" value="">
 			<input name="y" id="y" type="" value="">
+			<input name="windowSize" id="windowSize" type="" value="">
+			<input name="margin_left_of_center" id="margin_left_of_center" type="" value="">
 		</form>
 	</div>
 	<div class='float_left right_panel'  >
@@ -101,7 +130,7 @@
 	
 
 			<div  id='head-line-box' class="draggable" >
-				<div>
+				<div class='window-controls-container'>
 					<div class="handle icon-boxes"></div>
 				</div>
 				<div  class='insides' >
@@ -111,139 +140,158 @@
 			</div>
 			
 			<div  id='control-panel-box' class="draggable" >
-				<div class="handle icon-boxes"></div>
-				<div  class='close-window icon-boxes' >
+				<div  class='window-controls-container'>
+					<div class="handle icon-boxes">
+					</div>
+					<div  class='close-window icon-boxes' >
+					</div>					
+				</div>
+				<div  id='panel-tabs_container'>
+					<div>1
+					</div>
+					<div>2
+					</div>
+					<div>3
+					</div>
+					<div>4
+					</div>
 				</div>
 				<div  class='insides' >
-					<table>
-						<tr>
-							<td>Name
-							</td>
-							<td><input  name='full_name' id="full_name" type="" value="">
-							</td>
-						</tr>
-						
-						<tr>
-							<td>Background
-							</td>
-							<td>
-								<select  id='background_color' name='background_color'>
+					<div  id='panel-1'  class='panel ' >
+						<table>
+							<tr>
+								<td>Name
+								</td>
+								<td><input  name='full_name' id="full_name" type="" value="">
+								</td>
+							</tr>
+							
+							<tr>
+								<td>Background
+								</td>
+								<td>
+									<select  id='background_color' name='background_color'>
+										
+									<?php 
 									
-								<?php 
-								
-								$colors = array(
-									'red',
-									'blue',
-									'orange',
-									'yellow',
-									'green',
-									'white',
-								);
-								
-								foreach( $colors  as  $key => $color ){ ?>	
-
-										<option value='<?php  echo $color   ?>'><?php  echo $color   ?></option>
-								
-								<?php } ?>
-								
-								</select>
-							</td>
-						</tr>
-						<tr>
-							<td>Font Color
-							</td>
-							<td>
-								<select  id='font_color' name='font_color'>
+									$colors = array(
+										'red',
+										'blue',
+										'orange',
+										'yellow',
+										'green',
+										'white',
+									);
 									
-								<?php 
-								
-								$colors = array(
-									'black',
-									'red',
-									'blue',
-									'orange',
-									'yellow',
-									'green',
-									'white',
-								);
-								
-								foreach( $colors  as  $key => $color ){ ?>	
-
-										<option value='<?php  echo $color   ?>'><?php  echo $color   ?></option>
-								
-								<?php } ?>
-								
-								</select>
-							</td>
-						</tr>		
-						
-						<tr>
-							<td>Font Size
-							</td>
-							<td>
-								<select  id='font_size' name='font_size'>
+									foreach( $colors  as  $key => $color ){ ?>	
+	
+											<option value='<?php  echo $color   ?>'><?php  echo $color   ?></option>
 									
-								<?php 
-								
-								$sizes = array(
-									'9px',
-									'10px',
-									'11px',
-									'12px',
-									'13px',
-									'14px',
-									'15px',
-									'16px',
-									'17px',
-									'18px',
-									'19px',
-									'20px',
-									'21px',
-									'22px',
-									'23px',
-									'24px',
-									'25px',
-									'26px',
-									'27px',
-									'28px',
-									'29px',
-									'31px',
-									'32px',
-									'33px',
-									'34px',
-									'35px',
-									'36px',
-									'37px',
-									'38px',
-									'39px',
-									'40px',
-									'41px',
-									'42px',
-									'43px',
-									'44px',
-									'45px',
-									'46px',
-									'47px',
-									'48px',
-									'49px',
-									'50px',
-									'51px',
-									'52px',
-									'53px',
-									'54px',
-									'55px',
-								);
-								
-								foreach( $sizes  as  $key => $size ){ ?>	
+									<?php } ?>
+									
+									</select>
+								</td>
+							</tr>
+							<tr>
+								<td>Font Color
+								</td>
+								<td>
+									<select  id='font_color' name='font_color'>
+										
+									<?php 
+									
+									$colors = array(
+										'black',
+										'red',
+										'blue',
+										'orange',
+										'yellow',
+										'green',
+										'white',
+									);
+									
+									foreach( $colors  as  $key => $color ){ ?>	
+	
+											<option value='<?php  echo $color   ?>'><?php  echo $color   ?></option>
+									
+									<?php } ?>
+									
+									</select>
+								</td>
+							</tr>		
+							
+							<tr>
+								<td>Font Size
+								</td>
+								<td>
+									<select  id='font_size' name='font_size'>
+										
+									<?php 
+									
+									$sizes = array(
+										'9px',
+										'10px',
+										'11px',
+										'12px',
+										'13px',
+										'14px',
+										'15px',
+										'16px',
+										'17px',
+										'18px',
+										'19px',
+										'20px',
+										'21px',
+										'22px',
+										'23px',
+										'24px',
+										'25px',
+										'26px',
+										'27px',
+										'28px',
+										'29px',
+										'31px',
+										'32px',
+										'33px',
+										'34px',
+										'35px',
+										'36px',
+										'37px',
+										'38px',
+										'39px',
+										'40px',
+										'41px',
+										'42px',
+										'43px',
+										'44px',
+										'45px',
+										'46px',
+										'47px',
+										'48px',
+										'49px',
+										'50px',
+										'51px',
+										'52px',
+										'53px',
+										'54px',
+										'55px',
+									);
+									
+												foreach( $sizes  as  $key => $size ){ ?>	
+				
+														<option value='<?php  echo $size   ?>'><?php  echo $size   ?></option>
+												
+												<?php } ?>
+									
+									</select>
+								</td>
+							</tr>																	
+						</table>
+					<div  id='panel-2'   class='panel ' >
+						test						
+					</div>
+					</div>
 
-										<option value='<?php  echo $size   ?>'><?php  echo $size   ?></option>
-								
-								<?php } ?>
-								
-								</select>
-							</td>
-						</tr>																	
-					</table>
 				</div>
 			</div>
 </div>
@@ -263,13 +311,10 @@
 
 <script type="text/javascript" language="Javascript">
 	
-	
-x_start_position = 37;
-y_start_position = 51;
-
 
 	
 $(document).ready(function() { 
+
 	
 			get_stored_configurations();
 			
@@ -281,8 +326,15 @@ $(document).ready(function() {
 
 function get_stored_configurations(){
 	
-			$('#head-line-box').show().css({position:'relative',left:'<?php echo ( isset( $data['users'][0]->x ) ? $data['users'][0]->x:'0' )    ?>px', top:'<?php echo ( isset( $data['users'][0]->y) ? $data['users'][0]->y:'0' ) ?>px'})
-			
+
+			$('#head-line-box').css({
+				'position':'absolute',
+				'left':($(window).width() / 2),
+				'top':'<?php echo ( isset( $data['users'][0]->y) ? $data['users'][0]->y:'0' ) ?>px',
+				'margin-left':(0-<?php echo ( isset( $data['users'][0]->margin_left_of_center) ? $data['users'][0]->margin_left_of_center:'0' ) ?>)+'px',
+			})
+
+	
 			var full_name = '<?php echo ( isset( $data['users'][0]->full_name ) ? $data['users'][0]->full_name:'' )    ?>';
 			
 			$('#full_name').val( full_name );
@@ -305,85 +357,29 @@ function get_stored_configurations(){
 function store_custom_configuration(){
 	
 			$('#full_name').keyup(function(event) {
-				
 				$('#full_name_readonly').html( $(this).val() )	;	
-				
 			}).blur(function(event) {
-
-						$.post("<?php echo base_url(). 'index.php/home/update';    ?>",{
-							table:'users',
-							id:1,
-							set_what:$(this).serialize()
-							},function(data) {
-							
-								$('#y').val(data);
-		
-						});		
+						store( $(this) );		
 			});	
-			
 			
 			$('#background_color').change(function(event) {
-				
 						$('body').css({background:$(this).val()});
-						
-						$.post("<?php echo base_url(). 'index.php/home/update';    ?>",{
-							table:'users',
-							id:1,
-							set_what:$(this).serialize()
-							},function(data) {
-							
-								$('#y').val(data);
-								
-						});	
-						
+						store( $(this) );
 			});	
-			
 			
 			$('#font_color').change(function(event) {
-				
 						$('#full_name_readonly').css({color:$(this).val()});
-						
-						$.post("<?php echo base_url(). 'index.php/home/update';    ?>",{
-							table:'users',
-							id:1,
-							set_what:$(this).serialize()
-							},function(data) {
-							
-								$('#y').val(data);
-								
-						});	
-						
+						store( $(this) );
 			});				
 			
-			
 			$('#font_size').change(function(event) {
-				
 						$('#full_name_readonly').css({'font-size':$(this).val()});
-						
-						$.post("<?php echo base_url(). 'index.php/home/update';    ?>",{
-							table:'users',
-							id:1,
-							set_what:$(this).serialize()
-							},function(data) {
-							
-								$('#y').val(data);
-								
-						});	
-						
+						store( $(this) );	
 			});		
 			
-			
-			
 			$( "#head-line-box" ).mouseup(function(){
-
-
 						store_position( $(this) );
-						
-						
 			});	
-			
-			
-			
 
 }
 
@@ -391,7 +387,7 @@ function bind_events(){
 	
 			$( ".draggable" ).draggable({ 
 					handle: "div.handle",
-					containment: '#main-box'
+					// containment: '#main-box'
 			})
 			
 			$('#head-line-box .handle')
@@ -414,9 +410,17 @@ function bind_events(){
 			$( "#head-line-box" )
 			.mousemove(function(){
 				
-						var coord = $(this).position();
-						$('#x').val( coord.left.toFixed(0)  -  x_start_position );
-						$('#y').val( coord.top.toFixed(0) - y_start_position);
+						var coord = $(this).offset();
+						$('#x').val( coord.left.toFixed(0) );
+						$('#y').val( coord.top.toFixed(0));
+						
+	
+			$('#windowSize').val( $(window).width()  );
+			
+			$('#margin_left_of_center').val( ($(window).width()/2) -  (coord.left.toFixed(0)) ); 						
+
+
+						
 						
 		 	})
 			.mouseup(function(){
@@ -448,7 +452,7 @@ function bind_events(){
 				
 			
 			$('.close-window').click(function(event) {
-				$(this).parent().hide();
+				$(this).parent().parent().hide();
 			});	
 			
 
@@ -460,13 +464,26 @@ function bind_events(){
 
 }
 
-
+function store( dom_element ){
+	
+							$.post("<?php echo base_url(). 'index.php/home/update';    ?>",{
+							table:'users',
+							id:1,
+							set_what:dom_element.serialize()
+							},function(data) {
+							
+								$('#y').val(data);
+								
+						});	
+	
+}
 
 function store_position( dom_element ){
 	
-						var coord = dom_element.position();
-						$('#x').val( coord.left.toFixed(0) -  x_start_position );
-						$('#y').val( coord.top.toFixed(0) - y_start_position );
+						var coord = dom_element.offset();
+						$('#x').val( coord.left.toFixed(0) );
+						$('#y').val( coord.top.toFixed(0) );
+						$('#margin_left_of_center').val( ($(window).width()/2) -  (coord.left.toFixed(0)) ); 						
 
 						$.post("<?php echo base_url(). 'index.php/home/update';    ?>",{
 							table:'users',
