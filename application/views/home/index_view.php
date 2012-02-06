3bdf86ceeea2e7f843a7a3b981f77aea78d393ba
unknown <James Ming@JamesMing-THINK.(none)>
2012-02-06 12:04:14 -0800

    added

--- application/views/home/index_view.php
+++ application/views/home/index_view.php
@@ -57,19 +57,7 @@
 	border:0px solid gray;
 	}
 	
-	div#header .coordinates{
-		width:200px;
-	}
-	div#header .right_panel{
-	 width:662px;
-
-	}	
 	
-	div#header .right_panel .edit-panel{
-	 width:200px;
-	 background:lightgray;
-
-	}		
 	div#main-box{
 	min-height:600px;
 	}
@@ -1053,8 +1041,105 @@ $.fn.htmlbox=function(options){
 <html>
 
 <body>
-<div  id='header' class='container ' >
-	<div class='float_left coordinates '>
+<style>
+div#header .coordinates{
+	width:200px;
+}
+div#header div.sides{
+float:left;
+}	
+div#header .right_panel{
+background:white;
+margin-left:400px;
+}	
+
+div#header .right_panel .edit-panel{
+ width:200px;
+ background:lightgray;
+}
+div#header .right_panel #myaccount_container {
+	width:350px;
+	background:transparent;
+}
+
+				/* BEGIN TWITTER LOGIN CSS */
+				#myaccount_container #topnav {
+					font-size:13px;
+					line-height:23px;
+					text-align:right;
+					color:#8AA823;
+				}
+				#myaccount_container #topnav a.myaccount {
+					text-decoration:none;
+					font-size:12px;	
+					background:#A8C739;
+					padding:4px 6px 6px;
+					text-decoration:none;
+					font-weight:bold;
+					color:#F2F2F2;
+					border-top-left-radius: 5px;
+					-moz-border-radius-topleft: 5px;
+					-webkit-border-top-left-radius: 5px;
+					border-top-right-radius: 5px;
+					-moz-border-radius-topright: 5px;
+					-webkit-border-top-right-radius: 5px;					
+				}
+				#myaccount_container #topnav a.myaccount:hover {
+					background:#D5E8B4;
+					color:#8AA823!important;
+					text-decoration:none!important;
+				}
+				#myaccount_container #topnav a.myaccount, #topnav a.myaccount:hover {
+					*background-position:0 3px!important;
+				}
+				
+				#myaccount_container #topnav div#greeting{
+				font-size: 15px;
+				padding-right: 15px;
+				padding-top: 5px;
+				color:white;
+				}
+				
+				#myaccount_container #topnav a.myaccount {
+					position:relative;
+					margin-left:3px;
+				}
+				#myaccount_container #topnav a.myaccount span {
+					background-image:url(<?php echo base_url()    ?>images/twitter_login/toggle_down_light.png);
+					background-repeat:no-repeat;
+					background-position:100% 50%;
+					padding:4px 16px 6px 0;
+				}
+				#myaccount_container #topnav a.myaccount.menu-open {
+					background:#D5E8B4!important;
+					color:#666!important;
+					outline:none;
+				}
+				
+				#myaccount_container #topnav a.myaccount.menu-open span {
+					background-image:url(<?php echo base_url()    ?>images/twitter_login/toggle_up_dark.png);
+					color:#789;
+				}
+				
+				#myaccount_container #account_menu {
+						margin-top:-3px;
+				    border-bottom-left-radius: 5px;
+				    border-bottom-right-radius: 5px;
+				    border-top-left-radius: 5px;
+				    background-color: #D5E8B4;    
+				    color: gray;
+				    display: none;
+				    font-size: 12px;
+				    padding-top: 12px;
+				    padding-bottom: 12px;
+				    text-align: left;
+				    z-index: 1;
+				    width:76px;
+				}
+	
+</style>
+<div  id='header' class='container '  >
+	<div class='coordinates '>
 		<style>
 		#form0 input{
 		width:30px;	
@@ -1067,12 +1152,81 @@ $.fn.htmlbox=function(options){
 			<input name="margin_left_of_center" id="margin_left_of_center" type="" value="">
 		</form>
 	</div>
-	<div class='float_left right_panel'  >
-		<div  class='float_right edit-panel' >
-			<span  id='edit_mode' on='1'>preview</span>
-		</div>
+	<div class='right_panel'  >
+			<div  class='edit-panel float_left' >
+				<span  id='edit_mode' on='1'>preview</span>
+			</div>
+			<div  id='myaccount_container' class='float_left '  >
+		
+					<div id="topnav" class="topnav clearfix">
+						
+					 	<a  href="" class="float_right myaccount" onfocus="this.blur()">
+					 		<span>My Account</span>
+					 	</a>  
+					 		
+					 	<div id='greeting' class='float_right ' >Welcome TEST.
+					 	</div>
+					
+					</div>
+					<div  class=' clearfix ' >
+							<fieldset id="account_menu"    class='float_right '   >
+								<ul>
+									<li><a id='myprofile'>My Profile</a></li>
+									<li><a id='mypayment'>My Payment</a></li>
+									<li   style='display:none'  ><a id='mycredits'>My Credits</a></li>
+									<li><a id='mydeals'>My Deals</a></li>
+									<li   style='display:none'  ><a id='mypetgallery'>My Pet Gallery</a></li>
+									<li><a id='logout'>Log Out</a></li>
+								</ul>
+							</fieldset>						
+						
+					</div>
+	
+					
+			</div> 
 	</div>
 </div>
+<script type="text/javascript" language="Javascript">
+        $(document).ready(function() {
+
+            $("#account_menu a").click(function(e) {          
+							e.preventDefault();
+										
+								if( $(this).attr('id') == 'logout'){
+									
+									<?php if( $_SERVER['HTTP_HOST'] == 'localhost' ){?>
+											document.location.href='http://localhost/zekeszoo/index.php/home/index?logout=1';	
+									<?php }else{?>
+											// document.location.href='<?php echo  base_url().$_SERVER['REQUEST_URI'];   ?>?logout=1';									
+											document.location.href='<?php echo  base_url(); ?>home/index?logout=1';									
+									<?php } ?>
+
+								}else{
+									
+									document.location.href='<?php echo  base_url();   ?>index.php/home/'  + $(this).attr('id');									
+									
+								};
+            });
+
+            $(".myaccount").click(function(e) {          
+							e.preventDefault();
+			        $("fieldset#account_menu").toggle();
+							$(".myaccount").toggleClass("menu-open");
+            });
+
+						$("fieldset#account_menu").mouseup(function() {
+							return false
+						});
+						
+						$(document).mouseup(function(e) {
+							if($(e.target).parent("a.myaccount").length==0) {
+								$(".myaccount").removeClass("menu-open");
+								$("fieldset#account_menu").hide();
+							}
+						});		
+	
+        });
+</script>
 <div  id='main-box' class='container '>
 	
 
@@ -1092,6 +1246,7 @@ $.fn.htmlbox=function(options){
 		    height: 500px;
 		    width: 800px;
 		    margin: 50px;
+		    display:none;
 			}
 			#settings-box li.settings_tabs{
 			float:left;
@@ -1795,7 +1950,6 @@ $(document).ready(function() {
 
 			$('#profile_box #panel-tabs_container li#panel-tab-5').click()
 
-			
 
 });
 
