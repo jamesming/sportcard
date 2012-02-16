8db04f3f498e69504f2d10aa0ca29556da4e9097
unknown <James Ming@JamesMing-THINK.(none)>
2012-02-16 14:06:33 -0800

    added

--- application/controllers/card.php
+++ application/controllers/card.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Main extends CI_Controller {
+class Card extends CI_Controller {
 	
 	
    public function __construct(){
