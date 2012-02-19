2a86cee37fe3f555fbbe9c9212d465b39a9f3307
unknown <James Ming@JamesMing-THINK.(none)>
2012-02-18 22:27:35 -0800

    added

--- application/views/main/index_view.php
+++ application/views/main/index_view.php
@@ -92,8 +92,8 @@
 				
 				<div  id='profile_box' class="draggable box rounded"   style='
   -webkit-box-shadow: 0 0 12px #dbc59e;
-  -moz-box-shadow: 0 0 12px #dbc59e;
-  box-shadow: 0 0 12px #dbc59e;
+  -moz-box-shadow: 0 0 6px #dbc59e;
+  box-shadow: 0 0 6px #dbc59e;
 					'   >
 					
 					
